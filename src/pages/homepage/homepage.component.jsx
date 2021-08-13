904a6cac654cb05afd6e8803b28f85e912d708f5
aeropriest <ashok.jaiswal@gmail.com>
2021-08-13 12:21:30 +0800

    update our menu-items to nvaigate via withRouter hoc

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+
 import './homepage.styles.scss'
+
 import Directory from '../../components/directory/directory.components'
+
+
 const HomePage = () => {
     return (
     <div className='homepage'>
