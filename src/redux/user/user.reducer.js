e3d07aeefad6dade299843e0c54ac1984127207f
aeropriest <ashok.jaiswal@gmail.com>
2021-08-14 17:12:58 +0800

    Added the sign in and sign out hiding/shwoing

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,15 @@
+import { UserActionTypes } from './user.types'
+
 const INITIAL_STATE = {
     currentUser: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
+    console.log('user reducer was called')
+    console.log(action.type)
+    console.log(action.payload)
     switch(action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
