980092bac5a2984f8e116c7ec0093b5f457d5da9
aeropriest <ashok.jaiswal@gmail.com>
2021-08-17 13:47:51 +0800

    added redux for directory and shop, moved the data into selector instead of json

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,25 @@
+import './collections-overview.styles.scss'
+
+import { selectCollection } from '../../redux/shop/shop.selector';
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import CollectionPreview from '../preview-collection/collection-preview.component'
+
+const CollectionOverview = ({ collections }) => (
+    <div className='collections-overview'>
+             {
+                collections.map( ({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps}/>
+                ))
+            }
+       
+    </div>
+)
+
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollection
+})
+
+
+export default connect(mapStateToProps)(CollectionOverview)
--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,27 +1,9 @@
-import React, { Component }  from 'react';
-import SHOP_DATA from './shop.data.js'
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
-import '../../components/preview-collection/collection-preview.component'
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
-
-class ShopPage extends Component {
-    constructor(props) {
-        super(props)        
-        this.state = {
-            collections: SHOP_DATA
-        }
-        
-    }
-    render(){
-        const {collections} = this.state
-        return (<div className='shop-page'>
-            {
-                collections.map( ({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-        </div>)
-    }   
-}
+const ShopPage = ({ collections }) => (
+        <div className='shop-page'>
+           <CollectionOverview></CollectionOverview>
+        </div>
+)
 
 export default ShopPage
