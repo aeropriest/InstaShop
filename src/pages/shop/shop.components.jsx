cd8df16ed9e0d16f8cc7d86dab541707ea90f8d4
aeropriest <ashok.jaiswal@gmail.com>
2021-08-18 09:22:41 +0800

    added dircetory and shop redux

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,27 +1,22 @@
-import React, { Component }  from 'react';
-import SHOP_DATA from './shop.data.js'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect';
 
 import '../../components/preview-collection/collection-preview.component'
 import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import { selectCollections } from '../../redux/shop/shop.selector';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props)        
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({collections}) => (
+    <div className='shop-page'>
+        {
+            collections.map( ({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}/>
+            ))
         }
-        
-    }
-    render(){
-        const {collections} = this.state
-        return (<div className='shop-page'>
-            {
-                collections.map( ({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-        </div>)
-    }   
-}
+    </div>
+)
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage)
